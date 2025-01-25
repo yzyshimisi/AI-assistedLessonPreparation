ab1caaac43f197713fbeb7c11ebf11ce3b54ad3c
yzyshimisi <3198685096@qq.com>
2025-01-25 20:19:33 +0800

    add theme-change

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-  <router-view />
+<div class="h-screen">
+  <select data-choose-theme class="select select-bordered max-w-xs">
+    <option value="light">Light</option>
+    <option value="dark">Dark</option>
+    <option value="cupcake">Cupcake</option>
+    <option value="forest">Forest</option>
+  </select>
+</div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
+import { createApp } from "vue";
+import "tailwindcss/tailwind.css"
 import App from './App.vue'
 import router from "./router";
 import pinia from "./stores/createPinia";
+import { themeChange } from 'theme-change';
+
+themeChange(true);
 
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
 app.mount('#app')
+
 
